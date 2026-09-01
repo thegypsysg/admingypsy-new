472d4c445909a8304e9e8b70790a7d542e439ee4
Aji Prasetyo <ajiprsty4713@gmail.com>
2026-09-01 15:59:25 +0700

    fix(eslint): optimize ESLint extends and add .eslintignore to eliminate dev server warning flood
    - Replace plugin:prettier/recommended with eslint-config-prettier (prettier) in .eslintrc.js to prevent lintOnSave warning flood from legacy view files
    - Create .eslintignore file to exclude build outputs and third-party minified assets

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,12 @@ module.exports = {
     node: true,
     'vue/setup-compiler-macros': true,
   },
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'plugin:prettier/recommended'],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'prettier'],
   parserOptions: {
     parser: '@babel/eslint-parser',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'prettier/prettier': [
-      'warn',
-      {
-        semi: true,
-        singleQuote: true,
-        tabWidth: 2,
-        trailingComma: 'es5',
-        printWidth: 100,
-      },
-    ],
   },
 };
